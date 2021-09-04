bba7c8f3bf52f8e03bb032d84366cba14584d568
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-04 19:16:59 +0530

    ASHIQ added: WELCOME TO LINE COMPUTATION

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WELCOME TO LINE COMPUTATION
+        }
+    }
+}
